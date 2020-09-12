1da2d62edd35ae76d8a7b88c6a70d3603b73310d
canisaries <54444251+canisaries@users.noreply.github.com>
2020-09-12 16:17:22 +0300

    work on game board and panels begun template implemented, panels can be added dynamiically

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import "./styles.css";
 document.getElementById("script_text").innerHTML = `
 <p>this is the contents of the element "script_text", set to be this way from index.js</p>
 `;
+
+// Get game board
+const gameboard = document.getElementById("game_board");
+
+// A function that adds a button to the gameboard under the given id
+
+function addButton(button_id) {
+  var button_temp = document.getElementsByTagName("template")[0];
+  var button = button_temp.content.cloneNode(true);
+  button.id = button_id;
+  gameboard.appendChild(button);
+}
+
+var panel_amt = 16;
+
+for (var i = 0; i < panel_amt; i++) {
+  addButton("panel_" + i.toString());
+}
