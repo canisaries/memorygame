f5b367a9c28cf2034915ad20a6e98f5d4dc75de1
canisaries <54444251+canisaries@users.noreply.github.com>
2020-09-12 18:16:13 +0300

    starting to add functionality to panels its not going well

--- src/index.js
+++ src/index.js
@@ -7,17 +7,23 @@ document.getElementById("script_text").innerHTML = `
 // Get game board
 const gameboard = document.getElementById("game_board");
 
-// A function that adds a button to the gameboard under the given id
-
-function addButton(button_id) {
-  var button_temp = document.getElementsByTagName("template")[0];
-  var button = button_temp.content.cloneNode(true);
-  button.id = button_id;
-  gameboard.appendChild(button);
-}
-
 var panel_amt = 16;
 
 for (var i = 0; i < panel_amt; i++) {
-  addButton("panel_" + i.toString());
+  addPanel("panel_" + i.toString());
+}
+
+// A function that adds a panel to the gameboard under the given id
+function addPanel(panel_id) {
+  var panel_temp = document.getElementsByTagName("template")[0];
+  var panelfrag = panel_temp.content.cloneNode(true);
+  var panel = panelfrag.querySelector("button");
+  panel.setAttribute("id", panel_id.toString()); // Give panel its id
+  panel.setAttribute("onclick", "console.log(this.id)"); // Give panel its functionality
+  gameboard.appendChild(panelfrag);
+}
+
+// A function that flips the panel
+function flipPanel(panel_id) {
+  console.log(panel_id + "flipped!");
 }
